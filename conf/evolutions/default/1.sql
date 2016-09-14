b778c69295b2c76ce396e5553e8c4942af3e5d9f
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-09-14 23:14:23 +0200

    postgres syntax errors

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -2,9 +2,9 @@
 
 # --- !Ups
 
-CREATE TABLE Users (id bigint(20) SERIAL, email varchar(255) UNIQUE NOT NULL, password varchar(255) NOT NULL, role varchar(20) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE Users (id SERIAL, email varchar(255) UNIQUE NOT NULL, password varchar(255) NOT NULL, role varchar(20) NOT NULL, PRIMARY KEY (id));
 
-CREATE TABLE Notes (id bigint(20) SERIAL, owner bigint(20) NOT NULL REFERENCES Users(id), type varchar(20) NOT NULL, title varchar(50), text text, data bytea, createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, modifiedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id) );
+CREATE TABLE Notes (id SERIAL, owner bigint NOT NULL REFERENCES Users(id), type varchar(20) NOT NULL, title varchar(50), text text, data bytea, createdAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, modifiedAt timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY(id) );
 
 # --- !Downs
 
