b716c5ea8135119f8ab1bda0cc84653cebdcac96
anhminh1981 <anhminh.a.nguyen@gmail.com>
2016-09-26 22:54:52 +0200

    tests notes start

---
+++ test/NotesSpec.scala
@@ -0,0 +1,47 @@
+import org.scalatestplus.play._
+import play.api.test._
+import play.api.test.Helpers._
+import play.api.libs.json.Json
+import play.api.libs.json.JsObject
+
+
+class NotesSpec extends PlaySpec with OneAppPerSuite with TestData  {
+  insertData
+  val token = {
+    val requestBody = Json.obj( "email" -> "test@test.test", "password" -> "Test_123" )
+      
+    val request = new FakeRequest(POST, "/login", headers = FakeHeaders(Seq("Content-Type" -> "application/json")),
+        body =  requestBody )
+     
+    val result = route(app, request).get
+    
+    (contentAsJson(result) \ "token").as[String]
+  }
+  
+  "notes" should {
+    "refuse requests without authorization" in {
+      val request = FakeRequest(GET, "/notes")
+      
+      val result = route(app, request).get
+      
+      status(result) mustBe UNAUTHORIZED
+    }
+    
+    "return the list of notes owned by the user designated by the token" in {
+      val request = new FakeRequest(GET, "/notes", headers = FakeHeaders(Seq("Authorization" -> token)), body = "")
+      
+      val result = route(app, request).get
+      
+      status(result) mustBe OK
+      
+      contentType(result) mustBe Some("application/json")
+     
+      val json = contentAsJson(result)
+      
+      (json \ "notes").asOpt[Array[JsObject]] must not be None 
+      
+      (json \ "notes").as[Array[JsObject]].length mustBe 2
+    
+    }
+  }
+}
---
+++ test/TestData.scala
@@ -0,0 +1,45 @@
+import slick.profile.BasicProfile
+import play.api.db.slick.DatabaseConfigProvider
+import play.api.Application
+import dao.UserDao
+import dao.NoteDao
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import models._
+import scala.util.Success
+import scala.util.Failure
+import play.api.Logger
+import org.joda.time.Instant
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+
+trait TestData {
+  implicit val app: Application
+  object dbConfigProvider extends DatabaseConfigProvider {
+    def get[P <: BasicProfile] = DatabaseConfigProvider.get
+  }
+  val userDao = new UserDao(dbConfigProvider)
+  val noteDao = new NoteDao(dbConfigProvider)
+  private val now = Instant.now().toDateTime()
+  
+  def insertData = {
+	  val result = for(
+			  existing <- userDao.selectByEmail("test@test.test") if existing.isEmpty;
+			  user <- userDao.insert(User(0, "test@test.test", "Test_123", "user"));
+			  user2 <- userDao.insert(User(0, "test2@test.test", "Test_1223", "user"));
+			  note <- noteDao.insert(Note(0, user.id, "text", "title", "lore ipsum", Array[Byte](), now, now) ) ;
+			  note2 <- noteDao.insert(Note(0, user.id, "text", "title2", "lore ipsum", Array[Byte](), now, now) ) ;
+			  note3 <- noteDao.insert(Note(0, user2.id, "text", "title3", "lore ipsum", Array[Byte](), now, now) ) 
+			  ) yield 1
+
+	  result.onComplete {
+	    case Success(_) => {
+		    Logger.debug("Success creating test data" )
+	  }
+	    case Failure(t) => Logger.error("Error inserting test data: " + t.toString())
+	  }
+	  
+	  Await.result(result, 1 second)
+
+  }
+}
